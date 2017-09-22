1d0a3d74ea18f597a0dfeda82edd5e15388111f1
Nicholas Brown <znicholasbrown@gmail.com>
2017-09-22 16:31:33 -0400

    fixed regex

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get("/", function (req, res) {
-  let ipregex = /[,]/g,
+  let //ipregex = /[,]/g,
       lanregex = /[,]/g,
       softregex = /\(([^\)]+)\)/;
-  console.log(req.headers);
+
   let ipaddress = req.headers['x-forwarded-for'],
       language = req.headers['accept-language'],
       software = req.headers['user-agent'];
 
   let object = {
-    "ipaddress": ipaddress.slice(0, ipregex.exec(ipaddress).index),
+    "ipaddress": ipaddress,
     "language": language.slice(0, lanregex.exec(language).index),
     "software": software.match(softregex)[1]
   };
